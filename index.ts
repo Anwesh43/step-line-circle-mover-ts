d6328bfbbd846dc03f326ba9b4a2ab7bd5b29d3a
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 19:31:01 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -4,7 +4,12 @@ const backColor : string = "#BDBDBD"
 const fontFactor : number = 18
 const foreColor : string = "#3F51B5"
 const scGap : number = 0.02
-const delay : number = 50
+const delay : number = 20
+const lines : number = 5
+const rFactor : number = 5
+const lineCap = 'round'
+const strokeFactor = 90
+const nodes : number = 5
 
 class ScaleUtil {
 
@@ -18,6 +23,60 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawStepLineCircleMover(context : CanvasRenderingContext2D, i : number, w : number, scale : number, j : number) {
+        const gap : number = w / lines
+        const sf : number  = ScaleUtil.sinify(scale)
+        const sfi : number  = ScaleUtil.divideScale(scale, i, lines)
+        const sfi1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sfi2 : number = ScaleUtil.divideScale(scale, 1, 2)
+        const lineX2 : number = gap * sfi1
+        const r : number = gap / rFactor
+        const circleX  : number = gap * sfi2 + r
+        context.save()
+        context.translate(i * gap, 0)
+        DrawingUtil.drawLine(context, 0, 0, lineX2, 0)
+        if (i == j) {
+            DrawingUtil.drawCircle(context, circleX, 0, r)
+        }
+        context.restore()
+    }
+
+    static drawStepLineCircleMovers(context : CanvasRenderingContext2D, w : number, scale : number) {
+        const scDiv : number = 1 / lines
+        const j : number = ScaleUtil.sinify(scale) / scDiv
+        for (var i = 0; i < lines; i++) {
+            DrawingUtil.drawStepLineCircleMover(context, i, w, scale, j)
+        }
+    }
+
+    static drawSLCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const hGap = h / (nodes + 1)
+        context.lineCap = lineCap
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.fillStyle = foreColor
+        context.save()
+        context.translate(0, hGap * (i + 1))
+        DrawingUtil.drawStepLineCircleMovers(context, w, scale)
+        context.restore()
     }
 }
 
@@ -44,7 +103,8 @@ class Stage {
         this.context.font = this.context.font.replace(/\d+/g, `${fontSize}`)
         const text : string = `hello world`
         const tw : number = this.context.measureText(text).width
-        this.context.fillText(text, (w / 2  - tw / 2) * this.state.scale, h / 2 - fontSize / 2)
+        const sf : number = ScaleUtil.sinify(this.state.scale)
+        this.context.fillText(text, (w / 2  - tw / 2) * sf, h / 2 - fontSize / 2)
     }
 
     handleTap() {
