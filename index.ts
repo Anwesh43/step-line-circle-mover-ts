333d35d8869deb68911d71412fc8541b16c367b3
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 19:49:04 +0530

    adding SLCNode class

--- index.ts
+++ index.ts
@@ -170,3 +170,48 @@ class Animator {
         }
     }
 }
+
+class SLCNode {
+
+    state : State = new State()
+    prev : SLCNode
+    next : SLCNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SLCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLCNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SLCNode {
+        var curr : SLCNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
